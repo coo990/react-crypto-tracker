e4b4ee674b90fe9896ea6755eb3fdb0cd7190d9a
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-12-06 09:59:25 +0000

    feat: add coingecko api data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+      //Check data passing through
+      console.log(res.data)
+    }).catch(error => alert('Error API Error!'))
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Crypto Tracker</h1>
