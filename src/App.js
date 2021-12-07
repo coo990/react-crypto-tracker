5f9dc53b6193642e1f5a8a2d0d79a2b5daadd07f
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-12-07 16:11:23 +0000

    feat: add setSearch function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
   const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -13,18 +14,22 @@ function App() {
       //Check data passing through
       console.log(res.data)
     }).catch(error => console.log(error))
-  }, [])
+  }, []);
 
+  const handleChange = event => {
+    setSearch(event.target.value)
+  }
 
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text">Search a currency</h1>
         <form>
-          <input type="text" placeholder="Search" className="coin-input"/>
+          <input type="text" placeholder="Search" 
+          className="coin-input" onChange = {handleChange}/>
         </form>
       </div>
-      
+
     </div>
   );
 }
