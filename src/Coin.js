341a7d0cd3df72144d35c7b87477ca4a755654c6
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-12-07 16:32:59 +0000

    style: add coin symbole text

--- src/Coin.js
+++ src/Coin.js
@@ -7,14 +7,14 @@ const Coin = ({ name, image, symbol, price, volume, priceChange }) => {
         <div className="coin">
           <img src={image} alt="crypto"/>
           <h1>{name}</h1>
-          <p className="coin-symbol">{symbol}</p>
+          <p className="coin-symbol">Coin Symbol: {symbol}</p>
         </div>
         <div className="coin-data">
           <p className="coin-price">£{price}</p>
           <p className="coin-volume">£{volume.toLocaleString()}</p>
           {priceChange < 0 ? (
             <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
-          ) : (<p className="coin-percent GREEN">{priceChange.toFixed(2)}%</p>
+          ) : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>
           )}
         </div>
       </div>
