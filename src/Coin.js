26e4d22836e66bd65aa01deaf2bdbf04b9478e54
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-12-09 15:19:36 +0000

    style: add css styling

--- src/Coin.js
+++ src/Coin.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Coin.css'
 
 const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) => {
   return (
@@ -7,11 +8,11 @@ const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) =>
         <div className="coin">
           <img src={image} alt="crypto"/>
           <h1>{name}</h1>
-          <p className="coin-symbol">Coin Symbol: {symbol}</p>
+          <p className="coin-symbol">{symbol}</p>
         </div>
         <div className="coin-data">
-          <p className="coin-price">£{price}</p>
-          <p className="coin-volume">£{volume.toLocaleString()}</p>
+          <p className="coin-price">£{price} </p>
+          <p className="coin-volume"> £{volume.toLocaleString()}</p>
           {priceChange < 0 ? (
             <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
           ) : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>
